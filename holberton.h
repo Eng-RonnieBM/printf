6eb34864934fa77b510d952d7303ec304f20281c
ymcastellar <yoycas@hotmail.com>
2020-03-17 14:19:57 -0500

    modif header file

--- holberton.h
+++ holberton.h
@@ -4,8 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
+typedef struct call
+{
+	char *character;
+	int (*ptrfunc)();
+} caller;
+
+int print_c(va_list args);
+int print_s(va_list args);
+int print_p(va_list args);
 int _printf(const char *format, ...);
-
 
 #endif
