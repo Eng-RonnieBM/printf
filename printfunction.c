f3dba7a3721ae7f9a8b191afb4b1f53a311abbde
ymcastellar <yoycas@hotmail.com>
2020-03-17 14:54:47 -0500

    betty fixed

--- printfunction.c
+++ printfunction.c
@@ -7,38 +7,38 @@
 
 int _printf(const char *format, ...)
 {
-    int i = 0, j = 0, k = 0;
-    va_list args;
-    caller functions[] = {
+	int i = 0, j = 0, k = 0;
+	va_list args;
+	caller functions[] = {
 		{"c", print_c},
 		{"s", print_s},
 		{"%", print_p},
 		};
-    va_start(args, format);
-    while (format != NULL && format[i] != '\0')
-        {
-            switch(format[i])
-            {
-                case '%':
-                    j = 0;
-                    while (j < 3)
-                    {
-                        if (format[i + 1] == *functions[j].character)
-            			{
-                            k += functions[j].ptrfunc(args);
-                            i++;
-                            break;
-            			}
-            		j++;
-                    }
-                break;
-                
-                default:
-                write(1, format + i, 1);
-                k++;
-            }
-        i++;
-        }
-    va_end(args);
-    return(k);
+	va_start(args, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case '%':
+			j = 0;
+			while (j < 3)
+			{
+				if (format[i + 1] == *functions[j].character)
+				{
+					k += functions[j].ptrfunc(args);
+					i++;
+					break;
+				}
+				j++;
+			}
+			break;
+
+		default:
+			write(1, format + i, 1);
+			k++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (k);
 }
