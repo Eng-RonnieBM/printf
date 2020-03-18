6f881e3ab8220be3d96a30ee527d506e1e811a75
ymcastellar <yoycas@hotmail.com>
2020-03-18 17:15:20 -0500

    modif files

--- printfunction.c
+++ printfunction.c
@@ -7,44 +7,45 @@
  */
 int _printf(const char *format, ...)
 {
-	caller functions[] = {
-		{"c", print_c}, {"s", print_s},
-		{"%", print_p}, {NULL, NULL}
-		};
+	int i = 0, j = 0, k = 0;
 	va_list args;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	int k;
-
+	caller functions[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_p},
+		};
 	va_start(args, format);
-	k = 0;
-
 	if (format == NULL)
 		return (-1);
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		switch (format[i])
 		{
+		case '%':
 			j = 0;
-			while (functions[j].character != NULL)
+			while (j < 3)
 			{
-				if (format[i + 1] == *(functions[j].character))
+				if (format[i + 1] == *functions[j].character)
 				{
-					k += (functions[j].ptrfunc(args));
-					i += 1;
+					k += functions[j].ptrfunc(args);
+					i++;
 					break;
 				}
-				else
+				if (format[i + 1] == '\0')
+				{
+					return (-1);
+				}
 				j++;
 			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			k += 1;
+			break;
+
+		default:
+			write(1, format + i, 1);
+			k++;
 		}
 		i++;
 	}
 	va_end(args);
-	return (k);
+	return(k);
+
 }
